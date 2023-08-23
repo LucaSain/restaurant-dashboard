6f2ecb765213d1f2dff248235d90a974aeaeee13
Dovakiin_ <lucasainenco2@gmail.com>
2023-08-23 14:18:23 +0300

    Create orders.post.js

---
+++ server/api/orders.post.js
@@ -0,0 +1,11 @@
+import { Redis } from "@upstash/redis"
+const redis = Redis.fromEnv();
+
+export default defineEventHandler(async (event) => {
+    //get query params
+    const body = await readBody(event);
+    await redis.hset(body.id, { ...body });
+    await redis.lpush('curOrders', body.id);
+
+    return true;
+})
